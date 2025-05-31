084e0235d5d66f9cd7cae273130e22295f4b54e1
Bryant Paz <pazbryant7@outlook.com>
2025-05-31 11:36:32 -0600

    feat(fish,zsh): updating aliases

--- .config/fish/conf.d/02_aliases.fish
+++ .config/fish/conf.d/02_aliases.fish
@@ -1,9 +1,11 @@
 # main
 alias ls="ls"
 alias cl="clear"
+alias zshc="vi ~/zsh/.zshconfig"
 alias c="xclip -selection clipboard"
 alias vi="env NVIM_APPNAME='vim' nvim"
 alias p="xclip -selection clipboard -o"
+alias fishc="vi ~/.config/fish/config.fish"
 
 # system
 alias sht="shutdown now"
@@ -18,6 +20,6 @@ alias media="cd /run/media"
 alias yt='yt-dlp'
 alias yts='yt-dlp -o - --quiet | mpv --title="%(title)s" -'
 alias yta='yt-dlp -f bestaudio/best -x --embed-thumbnail --add-metadata --embed-subs'
-alias ytap='yt-dlp -f bestaudio/best -x --embed-thumbnail --add-metadata --embed-subs -o "%(playlist)s/%(title)s.%(ext)s"'
 alias ytv='yt-dlp -f "bestvideo[height<=2160]+bestaudio/best" --merge-output-format mp4'
+alias ytap='yt-dlp -f bestaudio/best -x --embed-thumbnail --add-metadata --embed-subs -o "%(playlist)s/%(title)s.%(ext)s"'
 alias ytvp='yt-dlp -f "bestvideo[height<=2160]+bestaudio/best" --merge-output-format mp4 -o "%(playlist)s/%(title)s.%(ext)s"'
