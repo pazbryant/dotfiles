47998a556e8a8dae79b78b5b71957d8f810f3748
Bryant Paz <pazbryant7@outlook.com>
2025-06-03 23:25:33 -0600

    feat(vim): update up to main neovim config

---
+++ .config/vim/lua/bryant/plugins/colorschemes.lua
@@ -0,0 +1,53 @@
+return {
+	{
+		'Mofiqul/vscode.nvim',
+		config = function()
+			require('vscode').setup({
+				transparent = false,
+				italic_comments = false,
+			})
+			vim.cmd.colorscheme('vscode')
+		end,
+	},
+
+	{
+		'rose-pine/neovim',
+		config = function()
+			require('rose-pine').setup({
+				styles = {
+					bold = true,
+					italic = false,
+					transparency = false,
+				},
+			})
+			vim.cmd.colorscheme('rose-pine')
+		end,
+	},
+
+	{
+		'craftzdog/solarized-osaka.nvim',
+		config = function()
+			require('solarized-osaka').setup({
+				transparent = false,
+				styles = {
+					comments = { italic = false },
+					keywords = { italic = false },
+				},
+			})
+			vim.cmd.colorscheme('solarized-osaka')
+		end,
+	},
+
+	{
+		lazy = false,
+		priority = 1000,
+		'rebelot/kanagawa.nvim',
+		config = function()
+			require('kanagawa').setup({
+				commentStyle = { italic = false },
+				keywordStyle = { italic = false },
+			})
+			vim.cmd.colorscheme('kanagawa')
+		end,
+	},
+}
