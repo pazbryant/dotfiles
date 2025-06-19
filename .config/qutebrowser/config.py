12063361740d0caf55ca8ae93d707a297497338c
Bryant Paz <pazbryant7@outlook.com>
2025-06-19 15:24:43 -0600

    feat(qutebrowser): adding base configuration

---
+++ .config/qutebrowser/config.py
@@ -0,0 +1,13 @@
+config.load_autoconfig()
+
+c.fonts.default_family = "sans-serif"
+c.fonts.default_size = "10pt"
+
+c.fonts.web.family.standard = "sans-serif"
+c.fonts.web.family.fixed = "monospace"
+c.fonts.web.family.serif = "serif"
+c.fonts.web.family.sans_serif = "sans-serif"
+c.fonts.web.family.cursive = "monospace"
+c.fonts.web.family.fantasy = "monospace"
+
+c.content.user_stylesheets = ["~/.config/qutebrowser/user.css"]
